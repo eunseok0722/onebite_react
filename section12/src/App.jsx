9e0e8a6dae983c5ec7885e4e0992fc9e26537f24
OES <eunseok0722@gmail.com>
2025-06-03 20:02:45 +0900

    update section12

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import {useReducer, useRef, createContext} from "react";
+import {Routes, Route, Link, useNavigate} from "react-router-dom";
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import NotFound from "./pages/NotFound";
+import Edit from "./pages/Edit";
 import Button from "./components/Button";
-import { getEmotionImage } from "./util/get-emotion-images.js";
+import Header from "./components/Header";
+import {getEmotionImage} from "./util/get-emotion-images.js";
 
 // 1. "/" : 모든 일기를 조회하는 Home
 // 2. "/new" : 새로운 일기를 작성하는 New
@@ -13,19 +16,96 @@ import { getEmotionImage } from "./util/get-emotion-images.js";
 // 4. "/diary/:id" : 특정 일기를 조회하는 Diary
 // 5. * :  와일드카드, 위 경로가 아닌 경우 404 페이지 보여주기
 
-function App() {
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date("2025-06-01").getTime(),
+    emotionId: 1,
+    content: "1번 일기내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date("2025-05-31").getTime(),
+    emotionId: 2,
+    content: "2번 일기내용",
+  },
+  {
+    id: 3,
+    createdDate: new Date("2025-06-02").getTime(),
+    emotionId: 3,
+    content: "3번 일기내용",
+  },
+];
 
-  // useNavigate 훅을 사용한 라우터 이동
-  const navigate = useNavigate();
-
-  const onClickButton = () => {
-    navigate("/new");
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) => (String(item.id) === String(action.data.id) ? action.data : item));
+    case "DELETE":
+      return state.filter((item) => String(item.id) !== String(action.targetId));
+    default:
+      return state;
   }
+}
+
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
+
+function App() {
+  // useNavigate 훅을 사용한 라우터 이동
+  // const navigate = useNavigate();
+
+  // const onClickButton = () => {
+  //   navigate("/new");
+  // }
+
+  const [data, dispatch] = useReducer(reducer, mockData);
+
+  // 일기 초기 데이터 3
+  const idRef = useRef(3);
+
+  // 새로운 일기 추가
+  const onCreate = (createdDate, emotionId, content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  // 기존 일기 수정
+  const onUpdate = (id, createdDate, emotionId, content) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  // 기존 일기 삭제
+  const onDelete = (targetId) => {
+    dispatch({
+      type: "DELETE",
+      targetId,
+    });
+  };
 
   return (
-
     <>
-      <Button text="New" />
+      {/* <Header title={"header"} leftChild={<Button text={"<"} />} rightChild={<Button text={">"} />} /> */}
+
+      {/* 버튼 클릭 이벤트 예시 */}
+      {/* <Button text={"New"} type={"POSITIVE"} onClick={onClickButton} /> */}
 
       {/* 이미지 import 예시 */}
       {/* <img src={getEmotionImage(1)} alt="emotion1" /> */}
@@ -36,16 +116,18 @@ function App() {
       {/* 버튼 클릭 이벤트 예시 */}
       {/* <button onClick={onClickButton}>New</button> */}
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/new" element={<New />} />
-        <Route path="/diary/:id" element={<Diary />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-
+      <DiaryStateContext.Provider value={data}>
+        <DiaryDispatchContext.Provider value={{onCreate, onUpdate, onDelete}}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/new" element={<New />} />
+            <Route path="/diary/:id" element={<Diary />} />
+            <Route path="/edit/:id" element={<Edit />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
     </>
-
-
   );
 }
 
--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -1,14 +1,47 @@
-import { useSearchParams } from "react-router-dom";
+// import { useSearchParams } from "react-router-dom";
+import {useState, useContext} from "react";
+import {useNavigate} from "react-router-dom";
+import {DiaryStateContext} from "../App";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import DiaryList from "../components/DiaryList";
+
+const getMonthlyData = (pivotDate, data) => {
+  // 1일 = 해당 월의 첫 날짜의 0시 0분 0초
+  const beginTime = new Date(pivotDate.getFullYear(), pivotDate.getMonth(), 1, 0, 0, 0);
+  // 0일 = 해당 월의 이전 달 마지막 날짜
+  const endTime = new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1, 0, 23, 59, 59);
+  return data.filter((item) => item.createdDate >= beginTime && item.createdDate <= endTime);
+};
 
 const Home = () => {
-    // 쿼리 파라미터 사용하는 방법
-    const [params, setParams] = useSearchParams();
+  // 쿼리 파라미터 사용하는 방법
+  // const [params, setParams] = useSearchParams();
 
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
-}
+  const data = useContext(DiaryStateContext);
+  const [pivotDate, setPivotDate] = useState(new Date());
+  const nav = useNavigate();
+
+  // 날짜 변경 함수
+  const onIncreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+  };
+  const onDecreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+  };
+
+  const monthlyData = getMonthlyData(pivotDate, data);
+
+  return (
+    <div>
+      <Header
+        title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`}
+        leftChild={<Button text={"<"} onClick={onDecreaseMonth} />}
+        rightChild={<Button text={">"} onClick={onIncreaseMonth} />}
+      />
+      <DiaryList data={monthlyData} />
+    </div>
+  );
+};
 
 export default Home;
